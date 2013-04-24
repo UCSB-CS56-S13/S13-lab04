798b03571a77b3cba7da65aa3e7c8049453aca2f
Fanny Kuang <fannykuang@umail.ucsb.edu>
2013-04-24 10:47:59 -0700

    Made first iteration. Test file mostly completed. All Tests failed

---
+++ src/edu/ucsb/cs56/S13/lab04/Yokura/Spaceship.java
@@ -0,0 +1,56 @@
+/** 4/24/13
+   @author Fanny Kuang
+*/
+
+package edu.ucsb.cs45.s13.lab04.Yokura;
+
+public class Spaceship {
+
+    private String destination;
+    private int numPeople;
+    
+    /** 
+     * No Argument constructor for Spaceship Class
+     */
+    public Spaceship() {}
+
+    /**
+     * Two argument constructor for Spaceship Class
+     */
+    public Spaceship(String destination, int numPeople) {}
+
+    /** 
+     * Get spaceship destination
+     */
+    public String getDestination() {}
+
+    /** 
+     * Get Number of spaceship people
+     */
+    public int getNumPeople() {}
+
+    /**
+     * Set new destination
+     * @param newDestination new spaceship destination
+     */
+    public void setDestination(String newDestination) {}
+
+    /**
+     * Set new number of people in spaceship
+     * @param newNum New number of people in spaceship
+     */
+    public void setNumPeople(int newNum) {}
+
+    public String toString() {}
+
+    /**
+     * Test against another spaceship class to see if they are equal
+     * @param someSpaceship  Spaceship object to compare to
+     */
+    public void equals(Spaceship someSpaceship) {};
+
+
+
+
+
+}
