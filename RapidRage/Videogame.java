57730a142c08e757e3aadb8f86b0d777013a741e
Chris Atanasian <catanasian@gmail.com>
2013-05-01 11:53:54 -0700

    added stubs

--- RapidRage/Videogame.java
+++ RapidRage/Videogame.java
@@ -1,5 +1,40 @@
 package edu.ucsb.cs56.S13.lab04.RapidRage;
 
 public class Videogame() {
+    private String name; // name of the videogame
+    private int yearReleased; // year that the videogame was released
+
+    public Videogame() {
+    
+    }
+
+    public Videogame(String name, int yearReleased) {
+	
+    }
+
+    public String getName() {
+	return null; // stub
+    }
+
+    public int getYearReleased() {
+	return -42; // stub
+    }
+
+    public void setName(String name) {
+	
+    }
+
+    public void setYearReleased(int yearReleased) {
+	
+    }
+
+    public String toString() {
+	return null; // stub
+    }
+
+    public boolean equals() {
+	return false; // stub
+    }
+
     
 }
