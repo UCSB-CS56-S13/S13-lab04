d1c249ada0e7d0517d5b4ddef4735014512231c5
Marc Lindsay <mlindsay@umail.ucsb.edu>
2013-05-01 11:53:02 -0700

    first commit

---
+++ mlindsay56/CoffeeTest.java
@@ -0,0 +1,34 @@
+package edu.ucsb.cs56.S13.lab04.mlindsay56;
+
+
+public class CoffeeTest(){
+
+    public CoffeeSetUp(){
+       	//this.typeOfCoffee = typeOfCoffee;
+	//this.height_inches = height_inches;
+    }
+    
+    public void test_getTypeOfCoffee(){
+	//this is a stub
+    }
+    
+    public void test_getHeight_inches(){
+	//this is a stub
+    }
+    
+    public void test_setTypeOfCoffee(String typeOfCoffee){
+	//DO NOTHING, THIS IS A STUB
+    }
+   
+    public void test_setHeight_inches(int height_inches){
+	//DO NOTHING, THIS IS A STUB
+    }
+
+    public void String test_toString(){
+	//this is stub
+     }
+    
+    public void Boolean test_equals(Coffee c){
+	//this is a stub
+     }
+    
