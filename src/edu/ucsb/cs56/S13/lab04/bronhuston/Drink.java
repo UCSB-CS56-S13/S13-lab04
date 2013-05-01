931991390ea59d9dde04434d7cf67692e9c09db7
Bronwyn Huston <bperryhuston@umail.ucsb.edu>
2013-05-01 11:51:42 -0700

    added Drink and DrinkTest with stubs and tests

---
+++ src/edu/ucsb/cs56/S13/lab04/bronhuston/Drink.java
@@ -0,0 +1,82 @@
+package edu.ucsb.cs56.S13.lab04.bronhuston;
+
+public class Drink{
+
+    private String drink_name;
+    private String size;
+    private int cost_in_dollars;
+
+    public Drink()
+    {
+        //stub
+        drink_name = "stub";
+        size = "stub";
+        cost_in_dollars = -42;
+    }
+
+    public Drink(String drink_name, String size, int cost_in_dollars)
+    {
+        //stub
+    }
+
+    public String getDrinkName()
+    {
+        return "stub";
+    }
+
+    public String getSize()
+    {
+        return "stub";
+    }
+
+    public int getCost()
+    {
+        return -42;
+    }
+
+    public void setDrinkName(String drink_name)
+    {
+        //stub
+    }
+
+    public void setSize(String size)
+    {
+        //stub
+    }
+
+    public void setCost(int cost_in_dollars)
+    {
+        //stub
+    }
+
+    public String toString()
+    {
+        return "stub";
+    }
+
+    public boolean equals(Object o)
+    {
+        //stub
+        return false;
+    }  
+
+    public static void main(String [] args) {
+
+        //stub!
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
