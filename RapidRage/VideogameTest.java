2a4e34c18e277fddb8b4be8e036e70dbc99bb1d6
Chris Atanasian <catanasian@gmail.com>
2013-05-01 21:20:17 -0700

    added all tests

--- RapidRage/VideogameTest.java
+++ RapidRage/VideogameTest.java
@@ -1,8 +1,7 @@
 package edu.ucsb.cs56.S13.lab04.RapidRage;
 
 import org.junit.Test;
-import org.junit.Before;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.AssertEquals;
 
 /** test class for Videogame
 
@@ -12,13 +11,12 @@ import static org.junit.Assert.assertEquals;
 
 */
 
-public class VideogameTest() {
+public class VideogameTest {
     
     /**
        test no arg constructor
        @see Videogame#Videogame()
     */
-    
     @Test public void testNoArgConstructor() {
 	// default Videogame should have an empty String for name and 0 for the yearReleased
 	Videogame v = new Videogame();
@@ -30,7 +28,6 @@ public class VideogameTest() {
        test the constructor that takes arguments
        @see Videogame#Videogame(String name, int yearReleased)
     */
-
     @Test public void testArgConstructor() {
 	Videogame v = new Videogame("Super Smash Bros. Melee", 2001);
 	assertEquals("Super Smash Bros. Melee",v.getName());
@@ -41,19 +38,56 @@ public class VideogameTest() {
        test case for getName()
        @see Videogame#getName()
     */
-    
-    @Test public String testGetName() {
+    @Test public void testGetName() {
 	Videogame v = new Videogame();
 	assertEquals("",v.getName());
     }
 
     /**
        test case for getYearReleased()
-       @see Videogame#getName()
+       @see Videogame#getYearReleased()
     */
-    
-    @Test public String testGetYearReleased() {
+    @Test public void testGetYearReleased() {
 	Videogame v = new Videogame();
 	assertEquals(0,v.getYearReleased());
-    }/
+    }
+
+    /**
+       test case for setName()
+       @see Videogame#setName()
+    */
+    @Test public void testSetName() {
+	Videogame v = new Videogame();
+	v.setName("Super Smash Bros. Melee");
+	assertEquals("Super Smash Bros. Melee",v.getName());
+    }
+    
+    /**
+       test case for setYearReleased()
+       @see Videogame#setYearReleased()
+    */
+    @Test public void testSetYearReleased() {
+	Videogame v = new Videogame();
+	v.setYearReleased(2001);
+	assertEquals(2001,v.getYearReleased());
+    }
+
+    /**
+       test case for toString()
+       @see Videogame#toString()
+    */
+    @Test public void testToString() {
+	Videogame v = new Videogame("Super Smash Bros. Melee", 2001);
+	assertEquals("Super Smash Bros. Melee, 2001",v.toString());
+    }
+    
+    /**
+       test case for equals()
+       @see Videogame#equals()
+    */
+    @Test public void testEquals() {
+	Videogame v1 = new Videogame("ABC",123);
+	Videogame v2 = new Videogame("ABC",123);
+	assertEquals(true,v1.equals(v2));
+    }
 }
