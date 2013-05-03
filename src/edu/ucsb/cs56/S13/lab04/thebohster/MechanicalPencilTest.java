696d79ec537647f6b274f27c6cfa11d764f0eac3
Bohan Lin <bohan_lin@yahoo.com>
2013-05-03 02:00:11 -0700

    fixed stubs

--- src/edu/ucsb/cs56/S13/lab04/thebohster/MechanicalPencilTest.java
+++ src/edu/ucsb/cs56/S13/lab04/thebohster/MechanicalPencilTest.java
@@ -8,42 +8,42 @@ import static org.junit.Assert.assertFalse;
 public class MechanicalPencilTest{
 
     @Test public void testMechanicalPencilNoArg(){
-	MechanicalPencil sample = new MechanicalPencil();
-	assertEquals("bic", "stub");
-	assertEquals(42, -42);
+        MechanicalPencil sample = new MechanicalPencil();
+        assertEquals("bic", sample.getBrand());
+        assertEquals(42, sample.getLeadSize());
     }
 
     @Test public void testMechanicalPencil2Args(){
-	MechanicalPencil sample = new MechanicalPencil("sample", 500);
-	assertEquals("sample", "stub");
-	assertEquals(500, -42);
+        MechanicalPencil sample = new MechanicalPencil("sample", 500);
+        assertEquals("sample", sample.getBrand());
+        assertEquals(500, sample.getLeadSize());
     }
 
     @Test public void testGetBrand(){
-	MechanicalPencil sample = new MechanicalPencil("sample", 500);
-	assertEquals("sample", "stub");
+        MechanicalPencil sample = new MechanicalPencil("sample", 500);
+        assertEquals("sample", sample.getBrand());
     }
 
     @Test public void testGetLeadSize(){
-	MechanicalPencil sample = new MechanicalPencil("sample", 500);
-	assertEquals(500, -42);
+        MechanicalPencil sample = new MechanicalPencil("sample", 500);
+        assertEquals(500, sample.getLeadSize());
     }
 
     @Test public void testSetBrand(){
-	MechanicalPencil sample = new MechanicalPencil("temp", 500);
-	sample.setBrand("bic");
-	assertEquals("bic", "stub");
+        MechanicalPencil sample = new MechanicalPencil("temp", 500);
+        sample.setBrand("bic");
+        assertEquals("bic", sample.getBrand());
     }
 
     @Test public void testSetLeadSize(){
-	MechanicalPencil sample = new MechanicalPencil("sample", 42);
-	sample.setLeadSize(700);
-	assertEquals(700, -42);
+        MechanicalPencil sample = new MechanicalPencil("sample", 42);
+        sample.setLeadSize(700);
+        assertEquals(700, sample.getLeadSize());
     }
 
     @Test public void testToString(){
-	MechanicalPencil sample = new MechanicalPencil("sample", 500);
-	assertEquals("sample 500um", "stub");
+        MechanicalPencil sample = new MechanicalPencil("sample", 500);
+        assertEquals("sample 500um", sample.toString());
     }
 
     @Test public void testEquals1(){
